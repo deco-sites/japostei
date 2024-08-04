42a4dfde7301b185b4ee106c3821ac824cd5b20e
fernando-rosa-wmw <fernando.rosa@wmw.com.br>
2024-08-04 16:49:10 -0300

    Seção 2

--- sections/SecondSection.tsx
+++ sections/SecondSection.tsx
@@ -1,27 +1,28 @@
-
 export default function SecondSection() {
-    return <div class="text-[#553410] flex flex-col gap-4 items-center justify-center text-center px-32">
-        <br></br>
-        <section>
-            <h1 class="text-3xl font-black">
-                Cansado(a) de perder horas criando conteúdo?
-            </h1>
-            <br></br>
-            <div class="text-2xl">
-                Você já sabe que precisa estar nas redes sociais para atrair mais seguidores e clientes para o seu negócio.
-            </div>
-            <br></br>
-            <div class="text-2xl">
-                Vê todos os seus concorrentes criando postagens incríveis e crescendo nas redes sociais mas o seu perfil parece não sair do lugar?
-            </div>
-            <br></br>
-            <div class="text-2xl">
-                Quer ter um perfil bonito e atrativo, mas passa horas no Canva e nada fica realmente interessante e profissional?
-            </div>
-            <br></br>
-            <div class="text-2xl">
-                Calma! Você não precisa mais passar horas do seu dia para fazer uma postagem ruim. Com o Canva Contabilidade você vai criar conteúdos profissionais em menos de 1 minuto.
-            </div>
-        </section>
-    </div>
+    return (
+        <div class="text-[#553410] flex flex-col gap-4 items-center justify-center text-center px-32">
+            <br />
+            <section>
+                <h1 class="text-3xl font-black">
+                    Cansado(a) de perder horas criando conteúdo?
+                </h1>
+                <br />
+                <div class="text-2xl">
+                    Você já sabe que <strong>precisa estar nas redes sociais para atrair mais seguidores e clientes</strong> para o seu negócio.
+                </div>
+                <br />
+                <div class="text-2xl">
+                    Vê todos os seus concorrentes criando postagens incríveis e crescendo nas redes sociais mas <strong>o seu perfil parece não sair do lugar?</strong>
+                </div>
+                <br />
+                <div class="text-2xl">
+                    <strong>Quer ter um perfil bonito e atrativo,</strong> mas passa horas no Canva e nada fica realmente interessante e profissional?
+                </div>
+                <br />
+                <div class="text-2xl">
+                    Calma! Você não precisa mais passar horas do seu dia para fazer uma postagem ruim. Com o <strong>Canva Contabilidade você vai criar conteúdos profissionais em menos de 1 minuto.</strong>
+                </div>
+            </section>
+        </div>
+    );
 }
